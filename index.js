bb511da0a582fdb2e8c349d175d35301e1886af6
thescriv <thescriv@student.42.fr>
2022-05-18 12:14:43 +0200

    firstCommit

---
+++ index.js
@@ -0,0 +1,54 @@
+const puppeteer = require('puppeteer')
+
+async function main() {
+    const browser = await puppeteer.launch({ headless: true });
+    const page = await browser.newPage();
+
+    let foundAppointments = false
+    let i = 0;
+    while (!foundAppointments) {
+        await page.goto('https://teleservices.paris.fr/rdvtitres/jsp/site/Portal.jsp?page=appointmentsearch&view=search&category=titres');
+
+        await page.waitForSelector('.container-fluid')
+
+        const bodyHandle = await page.$('.nextAvailableAppointments')
+
+        if (bodyHandle) {
+            foundAppointments = true
+
+            const res = await page.evaluate(() => {
+                const appointementPlace = document.querySelector('h4').innerHTML
+
+                const links = [...document.querySelectorAll('.nextAvailableAppointments a')]
+
+                const calendar = links.pop().href
+
+                const appointments = links.map(link => {
+                    return {
+                        schedule: link.innerHTML,
+                        booking: link.href
+                    }
+                })
+
+                return { appointementPlace, appointments, calendar }
+            }, bodyHandle)
+
+            console.log(`\n----------------------------------\nRendez-vous à [${res.appointementPlace}]`)
+            console.log(`Prochain rendez-vous :\n${res.appointments.map(({ schedule, booking }) => `    - ${schedule}\n [${booking}]`).join('\n')}`)
+            console.log(`\nCalendrier -> [${res.calendar}]`)
+        } else {
+            process.stdout.write(`\rWaiting${".".repeat(i)}${" ".repeat(4-i)}`)
+            if (i === 4) {
+                i = 0;
+            }
+            i++;
+
+            await new Promise(resolve => setTimeout(resolve, 1000));
+
+        }
+    }
+
+    await browser.close();
+}
+
+main()
